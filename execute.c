d02440fb93a3fc0ada2e928b8500655f55cd5a90
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-07-06 09:13:40 +0400

    updated

--- execute.c
+++ execute.c
@@ -6,26 +6,21 @@
  */
 void execute(char *line)
 {
-	pid_t pid;
-	char *args[2];
+    pid_t pid;
+    char *args[2];
 
-	args[0] = line;
-	args[1] = NULL;
+    args[0] = line;
+    args[1] = NULL;
 
-	pid = fork();
+    pid = fork();
+    if (pid == -1)
+        return;
 
-	if (pid == -1)
-	{
-		perror("fork");
-		return;
-	}
+    if (pid == 0)
+    {
+        execve(args[0], args, environ);
+        exit(0);
+    }
 
-	if (pid == 0)
-	{
-		execve(line, args, environ);
-		perror("execve");
-		exit(EXIT_FAILURE);
-	}
-
-		wait(NULL);
+    wait(NULL);
 }
