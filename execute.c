6e7355effe993db3622a642d0a46f206194f3795
luvishta <luvishta14@gmail.com>
2026-07-08 13:49:58 +0400

    updated execute.c

--- execute.c
+++ execute.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
  * execute - runs a command with arguments using a child process
  * @line: string containing the command and its arguments
@@ -18,20 +17,22 @@ void execute(char *line)
 			line[i] = '\0';
 			i++;
 		}
-
 		if (line[i] == '\0')
 			break;
-
 		args[arg_count] = &line[i];
 		arg_count++;
-
 		while (line[i] != '\0' && line[i] != ' ' && line[i] != '\t')
 			i++;
 	}
 	args[arg_count] = NULL;
-
 	if (arg_count == 0)
 		return;
+	args[0] = handle_path(args[0]);
+	if (args[0] == NULL)
+	{
+		perror("./hsh");
+		return;
+	}
 	pid = fork();
 	if (pid == -1)
 		return;
